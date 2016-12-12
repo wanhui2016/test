251b4f7321225d906d03f31d90168644cb80ddf8
wanhui2016 <wanhui95@163.com>
2016-12-12 18:09:25 +0800

    1.0

---
+++ mavens/src/main/java/mime/wanhui/mavens/App.java
@@ -0,0 +1,14 @@
+package mime.wanhui.mavens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("万辉的测试maven");
+    }
+}
